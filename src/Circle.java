07c619fa11438bcf3f62eadef23734c948aaa7ab
Mary Ganey <71738533+MaryGaney@users.noreply.github.com>
2024-06-18 19:34:09 -0400

    final notes

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Circle {
+public class Circle {
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.pow(this.radius,2) * Math.PI;
+    }
 }
--- src/CircleDemo.java
+++ src/CircleDemo.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class CircleDemo {
+import java.util.ArrayList;
+
+public class CircleDemo {
+
+    public static void m2(ArrayList<Circle> circles){
+        circles.remove(0);
+    }
+    public static void m1(Circle c){
+        Circle c2 = c;
+        c2.setRadius(5);
+    }
+
+    public static void main(String[] args) {
+        Circle c1 = new Circle(10);
+
+        System.out.println("area 0f c1 before calling m1 = " + c1.getArea());
+
+        m1(c1);
+
+        System.out.println("area of c1 after calling m1 = " + c1.getArea());
+
+        ArrayList<Circle> cList = new ArrayList<>();
+
+        Circle c2 = new Circle(1);
+        Circle c3 = new Circle(2);
+
+        cList.add(c1);
+        cList.add(c2);
+        cList.add(c3);
+
+        System.out.println("size of list before calling m2 = " + cList.size());
+        m2(cList);
+        System.out.println("size of list after calling m2 = " + cList.size());
+    }
 }
